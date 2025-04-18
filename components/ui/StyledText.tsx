7b563ca871ccf9826916eb4f2c8972334c503473
Matt Megenhardt <mrmegenhardt@gmail.com>
2025-04-18 18:18:37 -0400

    Fix: StyledText

--- components/ui/StyledText.tsx
+++ components/ui/StyledText.tsx
@@ -1,19 +1,17 @@
-import { Text, TextProps } from "react-native";
+import { Text, type TextProps } from "./Themed";
 
-export function SansSerifText(props: TextProps) {
-  return (
-    <Text
-      {...props}
-      style={[props.style, { fontFamily: "sans-serif" }]}
-    />
-  );
+export function SerifText(props: TextProps) {
+  return <Text {...props} variant="serif" />;
 }
 
-export function MonoText(props: TextProps) {
-  return (
-    <Text
-      {...props}
-      style={[props.style, { fontFamily: "monospace" }]}
-    />
-  );
+export function SansSerifText(props: TextProps) {
+  return <Text {...props} variant="sansSerif" />;
+}
+
+export function SerifBoldText(props: TextProps) {
+  return <Text {...props} variant="serif" fontWeight="bold" />;
+}
+
+export function SansSerifBoldText(props: TextProps) {
+  return <Text {...props} variant="sansSerif" fontWeight="bold" />;
 }
