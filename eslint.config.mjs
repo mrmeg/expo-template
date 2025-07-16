024de0882e54855e16c707f9a3efb75756029aa8
Matt Megenhardt <mrmegenhardt@gmail.com>
2025-07-16 11:44:49 -0400

    Update to Expo 53 and other deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,60 +12,64 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 });
 
-export default [{
-  ignores: [
-    "android/**/*",
-    "ios/**/*",
-    "dist/**/*",
-    "drizzle/**/*",
-    "node_modules/**/*",
-  ],
-}, ...compat.extends(
-  "eslint:recommended",
-  "plugin:react/recommended",
-  "plugin:@typescript-eslint/recommended",
-), {
-  plugins: {
-    react,
-    "@typescript-eslint": typescriptEslint,
+export default [
+  {
+    ignores: [
+      "android/**/*",
+      "ios/**/*",
+      "dist/**/*",
+      "drizzle/**/*",
+      "node_modules/**/*",
+    ],
   },
-
-  languageOptions: {
-    globals: {
-      ...Object.fromEntries(
-        Object.entries(globals.browser).filter(([key]) => key.trim() === key)
-      ),
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ),
+  {
+    plugins: {
+      react,
+      "@typescript-eslint": typescriptEslint,
     },
 
-    parser: tsParser,
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+    languageOptions: {
+      globals: {
+        ...Object.fromEntries(
+          Object.entries(globals.browser).filter(([key]) => key.trim() === key),
+        ),
+      },
 
-  settings: {
-    react: {
-      version: "detect",
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "react/no-unescaped-entities": "off",
+      "@/indent": ["warn", 2],
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/ban-ts-comment": "off",
+      indent: "off",
+      "linebreak-style": "off",
+      quotes: ["warn", "double"],
+      semi: ["error", "always"],
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
-
-  rules: {
-    "no-unused-expressions": "off",
-    "@typescript-eslint/no-unused-expressions": "off",
-    "react/no-unescaped-entities": "off",
-    "@/indent": ["warn", 2],
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-require-imports": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/ban-ts-comment": "off",
-    indent: "off",
-    "linebreak-style": "off",
-    quotes: ["warn", "double"],
-    semi: ["error", "always"],
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off"
-  },
-}];
+];
